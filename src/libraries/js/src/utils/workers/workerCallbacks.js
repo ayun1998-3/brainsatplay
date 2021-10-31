b9a2d55b81a85e0ce6e9518efae4ef97f788682e
Garrett <gflynn@usc.edu>
2021-10-31 14:51:44 -0700

    Fixed regex

--- src/libraries/js/src/utils/workers/workerCallbacks.js
+++ src/libraries/js/src/utils/workers/workerCallbacks.js
@@ -195,11 +195,10 @@ export class CallbackManager {
               if(typeof args[key] === 'string') {
                 let obj = JSON.parse(args[key]);
                 for(const prop in obj) {
-                  let regex = new RegExp('([a-zA-Z]\w*|\([a-zA-Z]\w*(,\s*[a-zA-Z]\w*)*\)) =>')
+                  let regex = new RegExp('([^\=].+) =>') //new RegExp('([a-zA-Z]\w*|\([a-zA-Z]\w*(,\s*[a-zA-Z]\w*)*\)) =>')
                   let func = (typeof obj[prop] === 'string') ? obj[prop].substring(0,10).includes('function') : false;
                   let arrow = (typeof obj[prop] === 'string') ? regex.test(obj[prop]) : false;
                   obj[prop] = ( func || arrow ) ? parseFunctionFromText(obj[prop]) : obj[prop];
-                  console.log(prop,func,arrow);
                 }
                 this[key] = obj; //variables will be accessible in functions as this.x or this['x']
                 if (this.threeUtil) this.threeUtil[key] = obj;
